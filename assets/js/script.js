65a05d1bdcbfa73c6d6c290fe3ee19a141678d8c
Amanda Lacroix <amanda_lacroix@hotmail.com>
2023-02-06 08:11:04 -0400

    update button css, add pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+//    	EVENTLISTENER (see Week 4 #11 JS ): on start button click then FUNCTION: the timer starts (see Week 4 #11 JS ) and a question is presented (prompt)
+
+
+
+
+// WHEN I answer a question
+// THEN I am presented with another question
+//    	PROMPT: go to the next question
+
+
+
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+//    	FUNCTION: when answer is incorrect then time is subtracted from the clock 
+
+
+
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// 	FUNCTION (if/else statement?): end game by a)timer reachs 0 or b)all questions answered 
+
+
+
+
+// WHEN the game is over
+// THEN I can save my initials and my score
+// 	FUNCTION: to input user score and save to the LOCAL STORAGE (see Week 4 #25 JS)
+
+
 
