24694da41db9f4fcf8023058a7e0259f5363dde4
Amanda Lacroix <amanda_lacroix@hotmail.com>
2023-02-09 12:05:37 -0400

    add if statement/function for when the game is over or the time runs out in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,57 @@
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
-//    	EVENTLISTENER (see Week 4 #11 JS ): on start button click then FUNCTION: the timer starts (see Week 4 #11 JS ) and a question is presented (prompt)
 
-        var startButton = document.getElementById("button");
-        var timerEl = document.querySelector(".timeclock");
-        var secondsLeft = 10;  
-        var score = document.querySelector(".scoreBoard");
-        var correct = 0;
 
-        startButton.addEventListener("click", startQuiz);
+    var startButton = document.getElementById("button");
+    var timerEl = document.querySelector(".timeclock");
+    var score = document.querySelector(".scoreBoard");
+    var secondsLeft = 60; 
+// ADD THIS TO LOCAL STORAGE? WHY IS THE TOTAL NOT INCREASING?
+    var correct = 0;
+    // localStorage.setItem("correct", correct);
+// Places incremented score on page
+    score.textContent= "Your total score is: " + correct + ".";
+    console.log 
 
-        score.textContent= "Your total score is: " + correct + ".";
 
-        // we need a function that sets the timer interval
-    function setTime() {
-        var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timerEl.textContent = secondsLeft + "!";
-    
-        if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        }
-    
-        }, 1000);
-    
-        }
-
+// EVENTLISTENER (see Week 4 #11 JS ): on start button click then FUNCTION: the timer starts (see Week 4 #11 JS ) and a question is presented (prompt)
+startButton.addEventListener("click", startQuiz);
 // we need a function to start the quiz and the timer
     function startQuiz(){
-    // WHY WONT THIS WORK!!!!
-    setTime();
+// WHY WONT THIS WORK!!!! (timer starts after the last question)
+    setTime()
     playQuiz();
     }
 
+// we need a function that sets the timer interval
+    function setTime(){
+    var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerEl.textContent = secondsLeft + "!";
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+    if(secondsLeft === 0) {
+    clearInterval(timerInterval);
+    alert("GAME OVER");
+    }
   
-    var playQuiz = function(){
-        // ask the question
-        var userChoice1 =window.prompt ("What colour is the sky? A) purple B) green C) blue")
+    }, 1000);
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+//  HOW TO DO I DO THIS?????
     
-        // if users presses cancel, end function
+    }
+  
+// WHEN I answer a question
+// THEN I am presented with another question
+// PROMPTS: we need prompt for the questions and IF statements to verify the correct answer. 
+   
+   function playQuiz(){  
+// ask the question
+    var userChoice1 =window.prompt ("What colour is the sky? A) purple B) green C) blue")
+
+// if users presses cancel, end function
     if (!userChoice1) {
         return;
     }
@@ -46,6 +59,7 @@
     userChoice1 = userChoice1.toUpperCase();
 
     if (userChoice1 === "C"){
+// incements the user score by 1 if correct
         correct ++;
         alert("Answer is Correct!");
 
@@ -53,79 +67,78 @@
         (userChoice1 === "A") || 
         (userChoice1 === "B")
     ){
+        secondsLeft -= 10
         alert("Incorrect Answer");
+        
+    }
+        questionTwo();
+         
     }
 
-    questionTwo();
 
-}
-
-var questionTwo = function(){
-    // ask the question
+    function questionTwo(){
+//we need to ask the question
     var userChoice2 =window.prompt ("What colcour is a bus? A) yellow  B) green C) blue")
 
-    // if users presses cancel, end function
-if (!userChoice2) {
+//if users presses cancel, end function
+    if (!userChoice2) {
     return;
-}
+    }
 
-userChoice2 = userChoice2.toUpperCase();
+    userChoice2 = userChoice2.toUpperCase();
 
-if (userChoice2 === "A"){
+    if (userChoice2 === "A"){
     correct ++;
     alert("Answer is Correct!");
 
-} else if (
+    } else if (
     (userChoice2 === "B") || 
     (userChoice2 === "C")
-){
+    ){
     alert("Incorrect Answer");
-}
+    }
 
-questionThree();
+    questionThree();
 
-}
+    }
 
-var questionThree = function(){
+    function questionThree() {
     // ask the question
     var userChoice3 =window.prompt ("What colcour is a an apple? A) yellow  B) red C) blue")
 
     // if users presses cancel, end function
-if (!userChoice3) {
+    if (!userChoice3) {
     return;
-}
+    }
 
-userChoice3 = userChoice3.toUpperCase();
+    userChoice3 = userChoice3.toUpperCase();
 
-if (userChoice3 === "B"){
+    if (userChoice3 === "B"){
     correct ++;
     alert("Answer is Correct!");
 
-} else if (
+    } else if (
     (userChoice3 === "A") || 
     (userChoice3 === "C")
-){
-    alert("Incorrect Answer");
-}
+    ){
+    alert("Incorrect Answer")
 
+    }
 
-}
+    }
 
-// WHEN I answer a question
-// THEN I am presented with another question
-// PROMPTS: we need prompt for the questions and IF statements to verify the correct answer. 
-
-
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-//    	FUNCTION: when answer is incorrect then time is subtracted from the clock 
-
-
-
-
-// WHEN all questions are answered or the timer reaches 0
+// WHEN all questions are answered
 // THEN the game is over
-// 	FUNCTION (if/else statement?): end game by a)timer reachs 0 or b)all questions answered 
+    function gameOver(){
+    alert("GAME OVER!")
+
+    }
+
+
+
+
+
+
 
 
 
@@ -133,4 +146,3 @@ if (userChoice3 === "B"){
 // WHEN the game is over
 // THEN I can save my initials and my score
 // 	FUNCTION: to input user score and save to the LOCAL STORAGE (see Week 4 #25 JS)
-
